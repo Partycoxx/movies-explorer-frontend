75bdfe5dee89d647d42359a1d9ebcb956eaaec65
Nikita <nikevdokimov34@gmail.com>
2021-04-08 21:25:14 +0300

    Minor fix

--- src/containers/SavedMovies/SavedMovies.js
+++ src/containers/SavedMovies/SavedMovies.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../components/Header/Header";
 import ContentBlock from "../../components/ContentBlock/ContentBlock";
 import Search from "../../components/Search/Search";
